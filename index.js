fb9ec38b8c96bd755265c367759b9bbdd5511ff9
RLjumber <ronaldjumber@gmail.com>
2023-01-18 13:13:26 -0500

    added first middleware

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
