82503d397fea72c81f2136a4f3d7ffedc1f12264
RLjumber <ronaldjumber@gmail.com>
2023-01-22 20:29:54 -0500

    added require user function in utils.js, cant get the message: under construction to appear

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const postsRouter = express.Router();
 const { getAllPosts } = require('../db')
+const { requireUser } = require("./utils");
 
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+    console.log("this worked!")
+    res.send({ message: "under construction!" });
+})
 
 postsRouter.use((req, res, next) =>
 {
